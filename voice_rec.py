b1f46b5d3fea4b346094af885dbb6fcb36a14c4e
Jakub Kosior <jakubjkosior@gmail.com>
2017-03-15 14:34:38 +0100

    voice recognition part

---
+++ voice_rec.py
@@ -0,0 +1,18 @@
+import speech_recognition as sp
+
+def recognizer():
+    rec = sp.Recognizer()
+    with sp.Microphone() as source:
+        print("say sth")
+        audio = rec.listen(source)
+        
+    try: 
+        return rec.recognize_google(audio)
+    except sp.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sp.RequestError as SR:
+        print("Could not request results from Google Speech Recognition service; {}".format(SR))
+
+
+if __name__ == "__main__":
+    print(recognizer())
