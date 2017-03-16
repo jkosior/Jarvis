e32c513d84aaaffa392cc31568daab9af9b39bc4
Jakub Kosior <jakubjkosior@gmail.com>
2017-03-16 20:25:11 +0100

    upgraded while loop

--- voice_rec.py
+++ voice_rec.py
@@ -1,19 +1,30 @@
 import speech_recognition as sp
-
+ 
 def recognizer():
     rec = sp.Recognizer()
     with sp.Microphone() as source:
         print("say sth")
         audio = rec.listen(source)
-    
+     
     try: 
         return rec.recognize_google(audio)
+         
     except sp.UnknownValueError:
         print("Google Speech Recognition could not understand audio")
     except sp.RequestError as SR:
         print("Could not request results from Google Speech Recognition service; {}".format(SR))
-
-
-if __name__ == "__main__":
-    while recognizer() != "close":
-        print(recognizer())
+ 
+ 
+flag = True
+while flag:
+    if recognizer() == "quit":
+        flag = False
+    elif recognizer() == "open file":
+        file = open("test.txt", "a")
+        new_flag = True
+        while new_flag: 
+            if recognizer() == "close file":
+                file.close()
+                new_flag = False 
+            else:
+                file.write(recognizer() + "\n")
